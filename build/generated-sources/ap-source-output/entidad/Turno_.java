56da4fd4c160916e7de59b6046dd0c6634f6cbda
CristhianV15 <cmvc99@gmail.com>
2022-07-17 22:29:31 -0500

    Internalización
    En apoderados y ciclo (add/list/edit)
    Palabras generales en el bundle
    Apoderado y ciclo modificados en plantilla.hxtml

--- build/generated-sources/ap-source-output/entidad/Turno_.java
+++ build/generated-sources/ap-source-output/entidad/Turno_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.6.1.v20150605-rNA", date="2022-07-17T16:24:58")
+@Generated(value="EclipseLink-2.6.1.v20150605-rNA", date="2022-07-17T22:24:27")
 @StaticMetamodel(Turno.class)
 public class Turno_ { 
 
